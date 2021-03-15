c8abc6d71874066523bfd4904d9bfc8ae738c807
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2021-03-15 15:03:47 +0700

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,8 @@
 #lang info
 (define collection "mixfix")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("rackunit-doc"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/mixfix.scrbl" ())))
 (define pkg-desc "Mixfix for Racket")
 (define version "0.0")
